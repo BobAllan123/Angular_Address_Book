7702742076d1460429b19fd6dc0823c1a17ef958
Bob Allan <boballan123@gmail.com>
2020-12-04 11:54:11 -0700

    added email address phone

--- Address-Book/src/app/mock-contacts.ts
+++ Address-Book/src/app/mock-contacts.ts
@@ -1,10 +1,10 @@
 import { Contact } from './contact';
 
 export const CONTACTS: Contact[] = [
-  { id: 1, name: 'Greg' },
-  { id: 2, name: 'Susan' },
-  { id: 3, name: 'Mark' },
-  { id: 4, name: 'Nancy' },
-  { id: 5, name: 'Tony' },
-  { id: 6, name: 'Lisa' }
+  { id: 1, name: 'Greg', address: "305 E 4th Ave, SLC, UT, 84103", email: "greg@gmail.com", phone: "801-444-5555" },
+  { id: 2, name: 'Susan', address: "555 E 4th Ave, SLC, UT, 84103", email: "susan@gmail.com", phone: "801-222-5555" },
+  { id: 3, name: 'Mark', address: "322 E 4th Ave, SLC, UT, 84103", email: "mark@gmail.com", phone: "801-444-6655" },
+  { id: 4, name: 'Nancy', address: "765 E 4th Ave, SLC, UT, 84103", email: "nancy@gmail.com", phone: "801-111-5555" },
+  { id: 5, name: 'Tony', address: "887 E 4th Ave, SLC, UT, 84103", email: "tony@gmail.com", phone: "801-444-7777" },
+  { id: 6, name: 'Lisa', address: "223 E 4th Ave, SLC, UT, 84103", email: "lisa@gmail.com", phone: "801-444-1111" }
 ];
