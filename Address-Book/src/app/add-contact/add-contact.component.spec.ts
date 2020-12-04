e005daa8b98587aa219476b21d57058d9a0891d4
Bob Allan <boballan123@gmail.com>
2020-12-04 13:19:41 -0700

    Added add-contact component

--- Address-Book/src/app/add-contact/add-contact.component.spec.ts
+++ Address-Book/src/app/add-contact/add-contact.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { DashboardComponent } from './dashboard.component';
+import { AddContactComponent } from './add-contact.component';
 
-describe('DashboardComponent', () => {
-  let component: DashboardComponent;
-  let fixture: ComponentFixture<DashboardComponent>;
+describe('AddContactComponent', () => {
+  let component: AddContactComponent;
+  let fixture: ComponentFixture<AddContactComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
+      declarations: [ AddContactComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(DashboardComponent);
+    fixture = TestBed.createComponent(AddContactComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- Address-Book/src/app/app.module.ts
+++ Address-Book/src/app/app.module.ts
@@ -3,13 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { ContactDetailComponent } from './contact-detail/contact-detail.component';
 import { ContactsComponent } from './contacts/contacts.component';
-import { MessagesComponent } from './messages/messages.component';
+
 
 
 import { AppRoutingModule } from './app-routing.module';
+import { AddContactComponent } from './add-contact/add-contact.component';
 
 @NgModule({
   imports: [
@@ -19,11 +19,11 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   declarations: [
     AppComponent,
-    DashboardComponent,
     ContactsComponent,
     ContactDetailComponent,
-    MessagesComponent
+    AddContactComponent,
   ],
   bootstrap: [ AppComponent ]
 })
+
 export class AppModule { }
